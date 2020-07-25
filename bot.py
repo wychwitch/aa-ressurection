b5c29233c13de622d10ccb3f565ff2a85d04b0bd
wych(witch) <wych@wychwit.ch>
2020-07-25 15:56:02 -0500

    add discordPrint to handle messaging channel

--- bot.py
+++ bot.py
@@ -7,3 +7,7 @@ import json
 from os import path
 
 # We should move the quote dictionary numbers to the dictionary itself, maybe with the key "num" that just stores an int. This further reduces the json bloat hgdhfdg
+
+#might be too clunky but i figured could be used for 90% of channel-response commands
+async def discordPrint(ctx, str):
+    await ctx.send(str)
