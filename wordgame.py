1ca7101504820866e7eec6ce056a02531ac8d813
Wych <wychwitchcraft@gmail.com>
2023-08-08 17:19:04 -0500

    Update wordgame.py

--- wordgame.py
+++ wordgame.py
@@ -5,21 +5,24 @@ import linecache
 import re
 import shelve
 				
-async def run(client, message):
-	
-	wordlistlines = 52794					#number of lines in the wordlist you're using
+async def run(client, message, wordlist):
+	wordlistlines = 0					#number of lines in the wordlist you're using
 	distance = 0						#reset all the values
 	winner = ''						
-	left = "aardvark"					
-	right = "zygote"
-	
+	left = ''
+	right = ''
+	with open('filename.txt', 'r') as f:
+		left = f.readlines()[0]
+		right = f.readlines()[-1]
+		wordlistlines = f.readlines().len()
+
 	linenumber = random.randint(0, wordlistlines)		#pick a random line number
-	answer = linecache.getline('words.txt',linenumber)	#linecache allows you to pull a single line instead of the entire file
+	answer = linecache.getline(wordlist,linenumber)	#linecache allows you to pull a single line instead of the entire file
 	answer = answer.replace('\n','')			#strip line breaks
 	
 	
 	
-	await client.send_message(message.channel, 'Guess the word between **'+left+'** and **'+right+'**!')
+	await client.send_message(message.channel, 'Guess the answer between **'+left+'** and **'+right+'**!')
 	while winner == '':
 		guess = await client.wait_for_message(channel=message.channel)
 		
@@ -30,11 +33,11 @@ async def run(client, message):
 			break
 			
 		if guess.content.startswith("!") == True and guess.content == "!refresh":
-			await client.send_message(message.channel, 'Guess the word between **'+left+'** and **'+right+'**!')
+			await client.send_message(message.channel, 'Guess the answer between **'+left+'** and **'+right+'**!')
 			
 		if guess.content.startswith("!") == True and guess.content == "!hint":
 			distance = await count_string(left,right)
-			await client.send_message(message.channel, str(distance)+' known words are between **'+left+'** and **'+right+'**!')
+			await client.send_message(message.channel, str(distance)+' range is between **'+left+'** and **'+right+'**!')
 			if distance < 200:
 				await client.send_message(message.channel, 'The answer begins with **'+answer[0]+answer[1]+answer[2]+'**')
 			#print(answer)				#cheat code here for debugging purposes	
@@ -44,9 +47,12 @@ async def run(client, message):
 					
 		if guess.content.startswith("!") == False and guess.author != client.user:
 			if guess.content == answer:
-				await client.send_message(message.channel, 'You are right! The word is **{}**'.format(answer))
+				await client.send_message(message.channel, 'You are right! The answer is **{}**'.format(answer))
 				winner = guess.author
-				await client.send_message(message.channel, 'http://www.merriam-webster.com/dictionary/'+answer)
+				if wordlist == "pokemon.txt":
+					await client.send_message(message.channel, 'http://www.merriam-webster.com/dictionary/{}_(Pokémon)'.format(answer.capitalize()))
+				else:
+					await client.send_message(message.channel, 'http://www.merriam-webster.com/dictionary/'+answer)
 				break
 			if guess.content != answer:
 				if await check_string(guess.content):
@@ -62,17 +68,17 @@ async def run(client, message):
 	
 	
 	if winner == 'Game Stopped':
-		await client.send_message(message.channel, '\n:eggplant:  Everyone loses! :eggplant:')
+		await client.send_message(message.channel, '\nGame stopped. The answer was '+answer)
 	else:
-		fmt = ':peach:  The winner is {} :peach:\n10 frickin dollarydoos have been awarded!'
+		fmt = 'The winner is {}!!'
 		await client.send_message(message.channel, fmt.format(winner.mention))
 		await addscore(client, message, winner)
 	await client.send_message(message.channel, "\nType `!word` to start a new game or `!help` for instructions on how to play.")
 	
 	
-async def check_string(w):
+async def check_string(w, wordlist):
 	if w.isalnum() and not (' ' in w):					
-		with open("words.txt") as f:				#check if a user submitted word is found in the wordlist
+		with open(wordlist) as f:				#check if a user submitted word is found in the wordlist
 			found = False
 			#print('Iterating...')
 			for line in f:  				#iterate over the file one line at a time(memory efficient)
@@ -85,8 +91,8 @@ async def check_string(w):
 	else:
 		return False
 
-async def count_string(w,x):						#finds the distance between two words. used for !hint
-	with open("words.txt") as myFile:
+async def count_string(w,x, wordlist):						#finds the distance between two words. used for !hint
+	with open(wordlist) as myFile:
 		for num, line in enumerate(myFile, 1):
 			if re.match("{}$".format(w),line):
 				leftnum = num
@@ -106,7 +112,7 @@ async def addscore(client, message, user):
 		score[user.id] = current + 10
 	else:
 		score[user.id] = 10
-	await client.send_message(message.channel, str(user.name)+" now has "+str(score[user.id])+" dollarydoos")
+	await client.send_message(message.channel, str(user.name)+" now has "+str(score[user.id])+"points")
 	score.sync()
 	score.close()
 	
